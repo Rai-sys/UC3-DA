fe8c434a6e51b0a2ed4f1542de6aef6ecf3002a1
Rai-sys <railaneserafim484@gmail.com>
2024-11-23 17:39:50 -0300

    add: pasta da aula 1

---
+++ aula1/aula1.js
@@ -0,0 +1,139 @@
+// ============ QUESTÃO 01 ======================================
+
+// let valor = 150;
+
+// if (valor > 100) {
+//     console.log('Você possui um desconto de 20%!');
+// }
+// else if (valor > 50) {
+//     console.log('Você possui um desconto de 10%!');
+// }
+// else {
+//     console.log('Você não possui nenhum desconto!');
+// }
+
+// ============ QUESTÃO 02 ======================================
+
+// let idade = 15;
+
+// if (idade > 18) {
+//     console.log('adulto:',idade);
+// }
+// else if (idade > 12 & idade < 18) {
+//     console.log('adolescente:', idade);
+// }
+// else if (idade < 12) {
+//     console.log('criança', idade);
+// }
+
+// ============ QUESTÃO 03 ======================================
+
+// let tempo = 27;
+
+// if (tempo > 30) {
+//     console.log('clima quente');
+// }
+// else if (tempo > 15 && tempo < 30) {
+//     console.log('clima agradável');
+// }
+// else if (tempo < 15) {
+//     console.log('clima frio')
+// }
+
+// ============ QUESTÃO 04 ======================================
+
+// let nota = 75;
+
+// if (nota > 90) {
+//     console.log("nota 'A'");
+// }
+// else if (nota > 70 && nota < 90) {
+//     console.log("nota 'B'");
+// }
+// else if (nota < 70) {
+//     console.log("nota 'C'");
+// }
+
+// ============ QUESTÃO 05 ======================================
+
+// let velocidade = 150;
+
+// if (velocidade > 120) {
+//     console.log('muito rápido');
+// }
+// else if (velocidade > 80 && velocidade < 120) {
+//     console.log('velocidade adequada');
+// }
+// else if (velocidade < 80) {
+//     console.log('muito lento');
+// }
+
+// ============ QUESTÃO 06 ======================================
+
+// let estoque = 35
+
+// if (estoque > 50) {
+//     console.log('estoque alto');
+// }
+// else if (estoque > 10 && estoque < 50) {
+//     console.log('estoque medio');
+// }
+// else if (estoque < 10) {
+//     console.log('estoque baixo');
+// }
+
+// ============ QUESTÃO 07 ======================================
+
+// let peso = 2
+
+// if (peso > 5) {
+//     console.log('pesado');
+// }
+// else if (peso > 1 && peso < 5) {
+//     console.log('peso medio');
+// }
+// else if (peso < 1) {
+//     console.log('peso leve');
+// }
+
+// ============ QUESTÃO 08 ======================================
+
+// let altura = 30
+
+// if (altura < 50) {
+//     console.log('baixa')
+// }
+// else if (altura > 50 && altura < 150) {
+//     console.log('media')
+// }
+// else if (altura > 150) {
+//     console.log('alta')
+// }
+
+// ============ QUESTÃO 09 ======================================
+
+// let idade = 15
+
+// if (idade < 10) {
+//     console.log('infantil')
+// }
+// else if (idade > 10 && idade < 18) {
+//     console.log('adolescente')
+// }
+// else if (idade > 18) {
+//     console.log('adulto')
+// }
+
+// ============ QUESTÃO 10 ======================================
+
+// let rendimento = 18
+
+// if (rendimento < 10) {
+//     console.log('rendimento baixo')
+// }
+// else if (rendimento > 10 && rendimento < 15) {
+//     console.log('rendimento medio')
+// }
+// else if (rendimento > 15) {
+//     console.log('rendimento alto')
+// }
